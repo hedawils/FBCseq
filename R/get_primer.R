e5c5b5b09410a0685aa4fc044a03d4433182b806
new name <hedawils@gmail.com>
2022-12-22 02:25:43 -0500

    Updating examples

--- R/get_primer.R
+++ R/get_primer.R
@@ -8,15 +8,15 @@
 #'
 #' @export
 #'
-#' @example
-#' get_primer("CCAGATGTCGGCATTATTAGGCCATCTTCTGGCACAGAAATAGGTGGCCGTGTCCGAGGCTGTCAGACTGTTCATTTTCAGATCCATCGTGGTCGAGGTTTTGGA", annealing = 62)
+#' @examples get_primer("CCAGATGTCGGCATTATTAGGCCATCTTCTGGCACAGAAATAGGTGGCCGTGTCCGAGGCTGTCAGACTGTTCATTTTCAGATCCATCGTGGTCGAGGTTTTGGA", annealing = 62)
+#' 
 #' ### "CCAGATGTCGGCATTATTAGGCCATCTTCT"
 
 get_primer <- function(sequence, annealing = 62){
   success <- FALSE
   while (!success) {
     sequence <- substr(sequence,1,nchar(sequence)-1)
-    DNA <- alphabetFrequency(DNAString(sequence))
+    DNA <- Biostrings::alphabetFrequency(Biostrings::DNAString(sequence))
     tm <- 64.9 + 41 * (DNA[2] + DNA[3] - 16.4)/(DNA[1] + DNA[2] + DNA[3] + DNA[4])
     success <- tm < annealing
   }
